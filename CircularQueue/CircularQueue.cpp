c46f63807f0c4344b05b2a160c1fc9dfd4640684
ImronSatrioPurnomo <satriogames2223@gmail.com>
2024-05-31 10:52:16 +0700

    ADD VOID DISPLAY

--- CircularQueue/CircularQueue.cpp
+++ CircularQueue/CircularQueue.cpp
@@ -52,6 +52,15 @@ public:
 			else FRONT = FRONT + 1;
 		}
 	}
+	
+	void display() {
+		int FRONT_position = FRONT;
+		int REAR_position = REAR;
 
+		if (FRONT == -1) {
+			cout << "Queue is empty\n";
+			return;
+		}
+	}
 
 };
