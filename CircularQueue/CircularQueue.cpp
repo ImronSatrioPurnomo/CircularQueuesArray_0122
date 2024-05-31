ed78d80ebe4efda791c310d44c277dc4aa35ced9
ImronSatrioPurnomo <satriogames2223@gmail.com>
2024-05-31 10:37:39 +0700

    add void remove

--- CircularQueue/CircularQueue.cpp
+++ CircularQueue/CircularQueue.cpp
@@ -12,19 +12,35 @@ public:
 	}
 
 	void insert() {
-		int nua;
+		int num;
 		cout << "enter a number: ";
-		cin >> nua;
+		cin >> num;
 		cout << endl;
+
+		if ((FRONT == 0 && REAR == max - 1) || (FRONT == REAR + 1)) {
+			cout << "\nQueue overflow\n";
+			return;
+		}
+
+		if (FRONT == -1) {
+			FRONT = 0;
+			REAR = 0;
+		}
+		else {
+			if (REAR == max - 1)
+				REAR = 0;
+			else
+				REAR = REAR + 1;
+
+		}
+		queue_array[REAR] = num;
 	}
 
-	if ((FRONT == 0 && REAR == max - 1) || (FRONT == REAR + 1)) {
-		cout << "\nQueue overflow\n";
-		return;
+	void remove() {
+		if (FRONT == -1) {
+			cout << "Queue underflow\n";
+		}
 	}
 
-	if (FRONT == -1) {
-		FRONT = 0;
-		REAR = 0;
-	}
+
 };
